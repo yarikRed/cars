4c9692bae6dceed00a3d2d7981563f123283097a
Yarik Red <yarik.red@gmail.com>
2020-03-12 14:32:22 +0200

    Added axios data fetching

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
+
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    state = { images: [] }
+
+    onSearchSubmit = (term) => {
+        unsplash.get('/search/photos', {
+            params: { query: term}
+        })
+        .then(response => {
+            this.setState({ images: response.data.results});
+        });
     }
 
     render(){
         return (
         <div className="ui container" style={{ marginTop: '10px'}}>
             <SearchBar onSubmit={this.onSearchSubmit}/>
+            Found: {this.state.images.length} images
         </div>
     )}
 };
